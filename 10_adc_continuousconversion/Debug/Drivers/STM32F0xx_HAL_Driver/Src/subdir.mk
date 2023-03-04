285343f77a734b30b31eff163e72555aa60a3555
Izzeddin Abunahla <Izzel@Izzys-MacBook.local>
2023-03-03 23:35:56 -0500

    Ran Tests for 5 and 10

--- 10_adc_continuousconversion/Debug/Drivers/STM32F0xx_HAL_Driver/Src/subdir.mk
+++ 10_adc_continuousconversion/Debug/Drivers/STM32F0xx_HAL_Driver/Src/subdir.mk
@@ -64,7 +64,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Drivers/STM32F0xx_HAL_Driver/Src/%.o Drivers/STM32F0xx_HAL_Driver/Src/%.su: ../Drivers/STM32F0xx_HAL_Driver/Src/%.c Drivers/STM32F0xx_HAL_Driver/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F070xB -DSTM32F070RBTx -DSTM32 -DSTM32F0 -DNUCLEO_F070RB -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I/Users/Izzel/Documents/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include -I/Users/Izzel/Documents/STM32F070RB/chip_headers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F070xB -DSTM32F070RBTx -DSTM32 -DSTM32F0 -DNUCLEO_F070RB -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include -I/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Drivers-2f-STM32F0xx_HAL_Driver-2f-Src
 
--- 5_uart_tx/Debug/Src/subdir.mk
+++ 5_uart_tx/Debug/Src/subdir.mk
@@ -22,7 +22,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F070RBTx -DSTM32 -DSTM32F0 -DNUCLEO_F070RB -DSTM32F070xB -c -I../Inc -I/Users/Izzel/Documents/STM32F070RB/chip_headers/CMSIS/Include -I/Users/Izzel/Documents/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32F070RBTx -DSTM32 -DSTM32F0 -DNUCLEO_F070RB -DSTM32F070xB -c -I../Inc -I/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include -I/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
