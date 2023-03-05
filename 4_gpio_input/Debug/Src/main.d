00e6edbe32463e88c1a68e525f1cf17f5ba69079
Izzeddin Abunahla <Izzel@Izzys-MacBook.local>
2023-03-04 20:03:54 -0500

    Commit from Local Repository

--- 4_gpio_input/Debug/Src/main.d
+++ 4_gpio_input/Debug/Src/main.d
@@ -1,15 +1,15 @@
 Src/main.o: ../Src/main.c \
- /Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
- /Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f070xb.h \
- /Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/core_cm0.h \
- /Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/cmsis_version.h \
- /Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/cmsis_compiler.h \
- /Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/cmsis_gcc.h \
- /Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
-/Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
-/Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f070xb.h:
-/Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/core_cm0.h:
-/Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/cmsis_version.h:
-/Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/cmsis_compiler.h:
-/Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Include/cmsis_gcc.h:
-/Users/Izzel/Documents/STM32Workspace/STM32\ Baremetal\ Workspace.zip_expanded/STM32\ Baremetal\ Workspace/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+ /Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ /Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f070xb.h \
+ /Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/core_cm0.h \
+ /Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/cmsis_version.h \
+ /Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/stm32f070xb.h:
+/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/core_cm0.h:
+/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/cmsis_version.h:
+/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/Users/Izzel/Documents/STM32Workspace/STM32F070RB/chip_headers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
